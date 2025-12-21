c6d50b9003d4e3c24ae5da56f8e067a8196d3cd1
JaskRendix <tur3a.giorgio.menzi@gmail.com>
2025-12-21 09:03:56 +0100

    tile renderer

--- pyscroll/tile_renderer.py
+++ pyscroll/tile_renderer.py
@@ -43,6 +43,7 @@ class TileRendererProtocol(Protocol):
         tile_view: Rect,
         buffer_surface: Surface,
     ) -> None: ...
+
     def clear_region(
         self, surface: Surface, area: Optional[RectLike] = None
     ) -> None: ...
@@ -54,53 +55,72 @@ class TileRenderer(TileRendererProtocol):
         data: PyscrollDataAdapter,
         clear_color: Optional[ColorRGB | ColorRGBA],
     ):
-        """
-        data: PyscrollDataAdapter
-        clear_surface: function(surface, area) -> None
-        """
         self.data = data
         self._clear_color = clear_color
-        # Define internal defaults
         self._rgba_clear_color: ColorRGBA = (0, 0, 0, 0)
         self._rgb_clear_color: ColorRGB = (0, 0, 0)
 
     def queue_edge_tiles(
         self, tile_view: Rect, dx: int, dy: int, buffer_surface: Surface
     ) -> list[tuple[int, int, int, Surface]]:
-        """
-        Returns a list of (x, y, layer, image) tuples.
-        Also clears the buffer regions where new tiles will be drawn.
-        """
+        # Early exit: no movement
+        if dx == 0 and dy == 0:
+            return []
+
         tw, th = self.data.tile_size
-        v = tile_view
+        v_left = tile_view.left
+        v_top = tile_view.top
+        v_right = tile_view.right
+        v_bottom = tile_view.bottom
+        v_width = tile_view.width
+        v_height = tile_view.height
+
+        get_images = self.data.get_tile_images_by_rect
+        clear_region = self.clear_region
+
         queue: list[tuple[int, int, int, Surface]] = []
-
-        def append(rect: RectLike) -> None:
-            # rect = (x, y, w, h) in tile coords
-            queue.extend(self.data.get_tile_images_by_rect(rect))
-
-            if buffer_surface is None:
-                return
-
-            # Convert tile rect → pixel rect
-            px = (rect[0] - v.left) * tw
-            py = (rect[1] - v.top) * th
-            pw = rect[2] * tw
-            ph = rect[3] * th
-
-            self.clear_region(buffer_surface, (px, py, pw, ph))
 
         # Horizontal movement
         if dx > 0:
-            append((v.right - 1, v.top, dx, v.height))
+            rect = (v_right - 1, v_top, dx, v_height)
+            queue.extend(get_images(rect))
+            if buffer_surface is not None:
+                px = (rect[0] - v_left) * tw
+                py = 0
+                pw = rect[2] * tw
+                ph = v_height * th
+                clear_region(buffer_surface, (px, py, pw, ph))
+
         elif dx < 0:
-            append((v.left, v.top, -dx, v.height))
+            rect = (v_left, v_top, -dx, v_height)
+            queue.extend(get_images(rect))
+            if buffer_surface is not None:
+                px = 0
+                py = 0
+                pw = rect[2] * tw
+                ph = v_height * th
+                clear_region(buffer_surface, (px, py, pw, ph))
 
         # Vertical movement
         if dy > 0:
-            append((v.left, v.bottom - 1, v.width, dy))
+            rect = (v_left, v_bottom - 1, v_width, dy)
+            queue.extend(get_images(rect))
+            if buffer_surface is not None:
+                px = 0
+                py = (rect[1] - v_top) * th
+                pw = v_width * tw
+                ph = rect[3] * th
+                clear_region(buffer_surface, (px, py, pw, ph))
+
         elif dy < 0:
-            append((v.left, v.top, v.width, -dy))
+            rect = (v_left, v_top, v_width, -dy)
+            queue.extend(get_images(rect))
+            if buffer_surface is not None:
+                px = 0
+                py = 0
+                pw = v_width * tw
+                ph = rect[3] * th
+                clear_region(buffer_surface, (px, py, pw, ph))
 
         return queue
 
@@ -110,36 +130,27 @@ class TileRenderer(TileRendererProtocol):
         tile_view: Rect,
         buffer_surface: Surface,
     ) -> None:
-        """
-        Draws all tiles in the queue onto the buffer surface.
-        """
         tw, th = self.data.tile_size
         ltw = tile_view.left * tw
         tth = tile_view.top * th
 
         self.data.prepare_tiles(tile_view)
 
-        buffer_surface.blits(
-            (
-                (image, (x * tw - ltw, y * th - tth))
-                for x, y, layer, image in tile_queue
-            ),
-            doreturn=False,
+        # Precompute generator outside the C call
+        blit_items = (
+            (image, (x * tw - ltw, y * th - tth)) for x, y, layer, image in tile_queue
         )
 
+        buffer_surface.blits(blit_items, doreturn=False)
+
     def redraw_all(self, tile_view: Rect, buffer_surface: Surface) -> None:
-        """
-        Full redraw of all tiles in the tile_view.
-        """
         tile_queue = self.data.get_tile_images_by_rect(tile_view)
         self.flush_tile_queue(tile_queue, tile_view, buffer_surface)
 
     def clear_region(self, surface: Surface, area: Optional[RectLike] = None) -> None:
-        """Clear the surface using the appropriate clear color."""
         if self._clear_color is not None:
             clear_color = self._clear_color
         else:
-            # Choose RGB or RGBA based on surface format
             has_alpha = surface.get_masks()[3] != 0
             clear_color = self._rgba_clear_color if has_alpha else self._rgb_clear_color
 
